e26499496ed9c2df357a177c5eed7e2768438a2d
Vadim Krepskiy <vadim.krepski@gmail.com>
2024-11-24 16:45:51 +0300

    Remove technologies and experience sections

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import './App.css';
 import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/main/Main";
 import {Projects} from "./layout/sections/projects/Projects";
-import {Technologies} from "./layout/sections/technologies/Technologies";
-import {Experience} from "./layout/sections/experience/Experience";
 
 function App() {
     return (
@@ -11,8 +9,6 @@ function App() {
             <Header/>
             <Main/>
             <Projects/>
-            <Technologies/>
-            <Experience/>
         </div>
     );
 }
