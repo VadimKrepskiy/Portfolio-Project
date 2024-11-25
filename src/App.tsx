ee911de7f3a5d00638b82c4f7f817091e16160d2
Vadim Krepskiy <vadim.krepski@gmail.com>
2024-11-25 23:16:43 +0300

    Add contacts component to layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Main} from "./layout/sections/main/Main";
 import {Projects} from "./layout/sections/projects/Projects";
 import {MyTechStack} from "./layout/sections/myTechStack/MyTechStack";
 import {Footer} from "./layout/footer/Footer";
+import {Contacts} from "./layout/sections/contacts/Contacts";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
             <Main/>
             <MyTechStack/>
             <Projects/>
+            <Contacts/>
             <Footer/>
         </div>
     );
